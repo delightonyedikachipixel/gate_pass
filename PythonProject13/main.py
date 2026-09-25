b9a4ec4b4faa04c552d7a5718ae64432c5586b6e
delightonyedikachipixel <orji.delight.onyedikachi@gmail.com>
2026-09-25 16:06:12 +0100

    updates

--- PythonProject13/main.py
+++ PythonProject13/main.py
@@ -1,6 +1,7 @@
 
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.database import Base, engine
 from app import models
@@ -21,6 +22,13 @@ app = FastAPI(
     title="Gate Pass System",
     description="Visitor & vehicle gate pass management API",
     version="0.1.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(user_routers.router)
